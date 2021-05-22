b30b18670460a3fb9360e0cbcb671244ecaee551
George Evangelou <george_evangelou@windowslive.com>
2021-05-22 23:51:08 +0300

    Improved error handling

--- app/src/main/java/chess/space/Point2D.java
+++ app/src/main/java/chess/space/Point2D.java
@@ -51,6 +51,7 @@ public interface Point2D extends Serializable {
     }
 
     public static Point2D from(final String str) {
+
         return builder()
                 .setX(Integer.parseInt(str.split(DELIMITER)[0]))
                 .setY(Integer.parseInt(str.split(DELIMITER)[1]))
--- app/src/main/java/chess/utilities/HumanMoveReaderAndExecutor.java
+++ app/src/main/java/chess/utilities/HumanMoveReaderAndExecutor.java
@@ -34,22 +34,30 @@ public class HumanMoveReaderAndExecutor {
         Preconditions.checkNotNull(userInput);
 
         if (!userInput.contains(DELIMITED)) {
+            LOGGER.warn("ERROR: Invalid user input");
             return null;
         }
 
-        final String[] splitInput = userInput.split(DELIMITED);
-        final String startPointString = splitInput[0];
-        final String targetPointString = splitInput[1];
+        final String startPointString;
+        final String targetPointString;
+        try {
+            final String[] splitInput = userInput.split(DELIMITED);
+            startPointString = splitInput[0];
+            targetPointString = splitInput[1];
+        } catch (final Exception e) {
+            LOGGER.warn("ERROR: Invalid user input");
+            return null;
+        }
 
         final Point2D startPoint = Point2D.from(startPointString);
         final Point2D targetPoint = Point2D.from(targetPointString);
 
         final AbstractPiece pieceChosen = this.board.getPiece(startPoint);
         if (pieceChosen == null) {
-            LOGGER.warn("No piece selected. Try again: ");
+            LOGGER.warn("ERROR: No piece selected. Try again: ");
             return null;
         } else if (!player.getPieces().containsKey(pieceChosen.getId())) {
-            LOGGER.warn("Wrong piece selected. Try again: ");
+            LOGGER.warn("ERROR: Wrong player's piece selected. Try again: ");
             return null;
 
         }
@@ -63,14 +71,11 @@ public class HumanMoveReaderAndExecutor {
     }
 
     public void readExecuteMove(final AbstractPlayer player) {
-        PieceToPoint2DMove pieceToPoint2DMove;
+        PieceToPoint2DMove pieceToPoint2DMove = null;
         do {
             LOGGER.info("Please input your move (e.g. 3,4 5,5):");
             final String userInput = consoleInputReader.getUserInput();
             pieceToPoint2DMove = this.parseAndInspectMove(userInput, player);
-//            if (pieceToPoint2DMove == null) {
-//                LOGGER.info("Please try again: ");
-//            }
         } while (pieceToPoint2DMove == null);
 
         pieceToPoint2DMove.getPiece().setPosition(pieceToPoint2DMove.getTargetPoint());
