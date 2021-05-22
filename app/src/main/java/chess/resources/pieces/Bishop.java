af0a4b00c0eee3e2106dcf4610fd679d18e9bd6c
George Evangelou <george_evangelou@windowslive.com>
2021-05-22 23:02:51 +0300

    Maybe correct Bishop valid moves

--- app/src/main/java/chess/resources/pieces/Bishop.java
+++ app/src/main/java/chess/resources/pieces/Bishop.java
@@ -5,6 +5,7 @@ import chess.constants.StringVisualRepresentationOfPieces;
 import chess.constants.ValuesOfPieces;
 import chess.execution.ChessGame;
 import chess.space.Point2D;
+import com.google.common.base.Preconditions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,23 +22,27 @@ public final class Bishop extends AbstractPiece {
 
     @Override
     public List<Point2D> getAccessiblePositionsIgnoringCollisions(final ChessGame game) {
+        Preconditions.checkNotNull(game);
+
         final List<Point2D> accessiblePositions = new ArrayList<>();
         for (int i = -BoardDimensions.SIZE_X; i < BoardDimensions.SIZE_X; i++) {
-            {
-                final int x = getPosition().getX() + i;
-                final int y = getPosition().getY() + i;
-                final Point2D point = Point2D.builder().setX(x).setY(y).build();
-                if (game.getBoard().isWithinBoard(point)) {
-                    accessiblePositions.add(point);
-                }
+            final int x = getPosition().getX() + i;
+            final int y = getPosition().getY() + i;
+            final Point2D point1 = Point2D.builder().setX(getPosition().getX() + i).setY(getPosition().getY() + i).build();
+            final Point2D point2 = Point2D.builder().setX(getPosition().getX() + i).setY(getPosition().getY() - i).build();
+            final Point2D point3 = Point2D.builder().setX(getPosition().getX() - i).setY(getPosition().getY() + i).build();
+            final Point2D point4 = Point2D.builder().setX(getPosition().getX() - i).setY(getPosition().getY() - i).build();
+            if (game.getBoard().isWithinBoard(point1)) {
+                accessiblePositions.add(point1);
             }
-            {
-                final int x = getPosition().getX() + i;
-                final int y = getPosition().getY() + i;
-                final Point2D point = Point2D.builder().setX(x).setY(y).build();
-                if (game.getBoard().isWithinBoard(point)) {
-                    accessiblePositions.add(point);
-                }
+            if (game.getBoard().isWithinBoard(point2)) {
+                accessiblePositions.add(point2);
+            }
+            if (game.getBoard().isWithinBoard(point3)) {
+                accessiblePositions.add(point3);
+            }
+            if (game.getBoard().isWithinBoard(point4)) {
+                accessiblePositions.add(point4);
             }
         }
         return List.copyOf(accessiblePositions);
